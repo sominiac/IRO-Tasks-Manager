a224484557912ef992ca4cabe5bd53b04914d3d0
Sergey Sh <sominiac@gmail.com>
2022-07-15 01:53:24 +0700

    build(react-router-dom): добавил роутинг

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/auth">Auth</Link>
+        </li>
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Auth from "./core/views/Auth";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
+  </BrowserRouter>
   </React.StrictMode>
 );
