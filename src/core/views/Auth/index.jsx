ecf1b6c70e9bca990d872318d8ccf3a0bc9ebb24
Sergey Sh <sominiac@gmail.com>
2022-07-15 01:55:55 +0700

    feat: обновил Auth view

--- src/core/views/Auth/index.jsx
+++ src/core/views/Auth/index.jsx
@@ -1,3 +1,12 @@
+import UserAPI from "../../../API/APISections/User.js"
+
+start();
+
+async function start() {
+    let { code } = await UserAPI.auth();
+
+    console.log(code)
+}
 
 function Auth() {
     return (
