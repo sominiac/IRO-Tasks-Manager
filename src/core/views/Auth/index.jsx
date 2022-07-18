8a3f7ef24289a78f33dff1bf550c18bbef4c2a1b
Sergey Sh <sominiac@gmail.com>
2022-07-18 19:08:07 +0700

    feat: добавил отображения нескольких тем и дополнил стили к авторизации

--- src/core/views/Auth/index.jsx
+++ src/core/views/Auth/index.jsx
@@ -1,16 +1,53 @@
-import UserAPI from "../../../API/APISections/User.js"
-
-start();
-
-async function start() {
-    let { code } = await UserAPI.auth();
-
-    console.log(code)
-}
+import { AuthPageStyle } from "../../../assets/styles/pages/Auth/AuthMain";
+import { InputWithRightBorder, InputWithLeftBorder, Button, SecondaryButton } from "../../../assets/styles/components/_index";
 
 function Auth() {
     return (
-        <h1>Auth page</h1>
+        <div className="auth">
+            <AuthPageStyle /> {/* Подключаем глобальные стили страницы */}
+
+            <div className="auth__wrapper">
+                <div className="auth__login">
+                    <div className="auth__welcome">
+                        <div className="auth__logo">
+                            <img className="auth__logo-icon" />
+                        </div>
+
+                        <span className="auth__welcome-text">Welcome<br />Back!</span>
+                    </div>
+
+                    <div className="auth__title">
+                        <h1 className="auth__title-text">Авторизоваться</h1>
+                    </div>
+
+                    <div className="auth__login-form">
+                        <div className="auth__login-item auth__login-item_login">
+                            <span className="auth__login-lable">Введите email</span>
+                            <InputWithRightBorder type="email" />
+                        </div>
+
+                        <div className="auth__login-item auth__login-item_password">
+                            <span className="auth__login-lable">Введите пароль</span>
+                            <InputWithLeftBorder type="password" />
+                        </div>
+
+                        <div className="auth__login-forget">
+                            <a className="auth__login-link" href="#">Забыли пароль?</a>
+                        </div>
+
+                        <div className="auth__login-wrapper-button">
+                            <Button>Войти</Button>
+                        </div>
+                    </div>
+
+                    <div className="auth__register">
+                        <span className="auth__register-text">Еще не зарегистрированы?</span>
+
+                        <SecondaryButton>Зарегистрироваться</SecondaryButton>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from "styled-components";
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
 
+import { light, purpleDark } from "./assets/styles/themes/Theme.styled";
+import { GlobalStyles } from "./assets/styles/global/Global";
+
 import Auth from "./core/views/Auth";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/auth" element={<Auth />} />
-    </Routes>
-  </BrowserRouter>
+    <ThemeProvider theme={purpleDark}>
+      <GlobalStyles />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
